73ce304258eeee6b83ba2bd4a1c159cb08d2cb3b
Aditya Gupta <20061006adityagupta@gmail.com>
2026-02-05 23:16:16 +0530

    chapter 2 variables

---
+++ primitive_datatypes.cpp
@@ -0,0 +1,16 @@
+// primitive date types
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 25;          // 4 byte
+    char grade = 'a';      // 1 byte
+    float pi = 3.14;       // 4 byte
+    bool isSafe = false;   // 1 byte
+    double price = 100.99; // 8 bytes
+    cout << age;
+
+    return 0;
+}
---
+++ type_casting.cpp
@@ -0,0 +1,12 @@
+// type casting - manual conversion of data from one type to other
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double price= 100.99;
+    int NewPrice=(int)price;
+    cout<< NewPrice;
+
+    return 0;
+}
